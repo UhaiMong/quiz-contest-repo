845cd20fcfed7a8c54cf00a0e3a25f048525dfd3
UhaiMong <uhaimarma2016@gmail.com>
2022-10-11 10:22:46 +0600

    react router dom install

---
+++ src/Components/Statistics/Statistics.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Statistics = () => {
+    return (
+        <div>
+            <h2>Statistics component</h2>
+        </div>
+    );
+};
+
+export default Statistics;

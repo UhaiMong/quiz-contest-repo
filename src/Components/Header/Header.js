002e4ecdf3dac6e4fe124f82c8ab6872ce443334
UhaiMong <uhaimarma2016@gmail.com>
2022-10-11 10:36:02 +0600

    router setup completed

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import './Header.css'
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <div>
-            <h1>Header</h1>
-        </div>
+        <nav>
+            <div className="header-title">
+                <h3>Quiz Contest</h3>
+            </div>
+            <div className="nav-rout">
+                <Link to='/topics'>Topics</Link>
+                <Link to='/statistics'>Statistics</Link>
+                <Link to='/blogs'>Blogs</Link>
+            </div>
+        </nav>
     );
 };
 
